fe9e8f7bbb299b463b30843cacce7199e44d2aa9
Maria Novozhilova <maria.nowozhilova@gmail.com>
2024-07-08 23:06:17 +0300

    Script for copying data from source tables into ods layer. Iterates the list of tables.

--- script_1.18.py
+++ script_1.18.py
@@ -1,16 +1,19 @@
 import psycopg
+import json
 
-tables = ['''
-         'базы_данных', 'базы_данных_и_уровень_знаний_сотру', 'инструменты', 'инструменты_и_уровень_знаний_сотр',
+tables = ['базы_данных', 'базы_данных_и_уровень_знаний_сотру', 'инструменты', 'инструменты_и_уровень_знаний_сотр',
          'образование_пользователей', 'опыт_сотрудника_в_отраслях', 'опыт_сотрудника_в_предметных_обла', 
          'отрасли', 'платформы', 'платформы_и_уровень_знаний_сотруд', 'предметная_область', 'резюмедар', 
          'сертификаты_пользователей', 'сотрудники_дар', 'среды_разработки', 'среды_разработки_и_уровень_знаний_', 
-         'технологии' , 'технологии_и_уровень_знаний_сотру', 'типы_систем', 'типы_систем_и_уровень_знаний_сотру', 
+         'технологии', 'технологии_и_уровень_знаний_сотру', 'типы_систем', 'типы_систем_и_уровень_знаний_сотру', 
          'уровень_образования', 'уровни_владения_ин', 'уровни_знаний', 'уровни_знаний_в_отрасли', 
          'уровни_знаний_в_предметной_област', 'фреймворки', 'фреймворки_и_уровень_знаний_сотру', 'языки',
-         'языки_пользователей', 'языки_программирования', 'языки_программирования_и_уровень'
-'''
-]
+         'языки_пользователей', 'языки_программирования', 'языки_программирования_и_уровень']
+
+f = open('source_base.json')
+g = open('etl_base.json')
+dsn_source = json.load(f)
+dsn_ods = json.load(g)
 
 def create_connection(db_name, db_user, db_password, db_host, db_port):
     connection = None
@@ -27,28 +30,22 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
         print(f"The error '{e}' occurred")
     return connection
 
-connection_source = create_connection(db_name = 'source', db_user = 'etl_user_7', db_password = 'MjT3-%>s', db_host = '10.82.0.4', db_port = '5432')
-connection_new = create_connection(db_name = 'etl_db_7', db_user = 'etl_user_7', db_password = 'MjT3-%>s', db_host = '10.82.0.4', db_port = '5432')
+connection_source = create_connection(dsn_source["db_name"], dsn_source["db_user"], dsn_source["db_password"], dsn_source["db_host"], dsn_source["db_port"])
+connection_new = create_connection(dsn_ods["db_name"], dsn_ods["db_user"], dsn_ods["db_password"], dsn_ods["db_host"], dsn_ods["db_port"])
 
+def write_data(table):
+    print(f'Copy data from {table}')
+    with connection_source as conn1, connection_new as conn2:
+        with conn1.cursor().copy(f'COPY source_data."{table}" TO STDOUT') as copy1:
+            with conn2.cursor().copy(f'COPY ods."{table}" FROM STDIN') as copy2:
+                for data in copy1:
+                    copy2.write(data)
 
-with connection_source as conn1, connection_new as conn2:
-    conn2.cursor().execute('''
-                CREATE TABLE IF NOT EXISTS etl_db_7."базы_данных" (
-     	        "название" character varying(50) COLLATE pg_catalog."default",
-     "активность" character varying(50) COLLATE pg_catalog."default",
-     "Сорт." integer,
-     "Дата изм." character varying(50) COLLATE pg_catalog."default",
-     id integer
-);
-                ''')
-    with conn1.cursor().copy('COPY source_data."базы_данных" TO STDOUT') as copy1:
-        with conn2.cursor().copy('COPY etl_db_7."базы_данных" FROM STDIN') as copy2:
-            for data in copy1:
-                copy2.write(data)
-
+for table in tables:
+    write_data(table)
 
 connection_new.commit()
-cur_new.close()
 connection_new.close()
 connection_source.close()
 
+#if __name__ == "__main__":
