e9158f39838ad3660ff9f8a30ca303925615151e
Maria Novozhilova <maria.nowozhilova@gmail.com>
2024-07-23 15:45:37 +0300

    some column names updated table total_certificates deleted as long as the data included into personal data

--- DM_tables.py
+++ DM_tables.py
@@ -25,20 +25,7 @@ conn = create_connection(dsn_source["db_name"], dsn_source["db_user"], dsn_sourc
 
 tables = ['personal_data', 'total_certificates', 'employee_skill']
 
-SQL_DM_dict = {'total_certificates': '''
-TRUNCATE TABLE dm.total_certificates; INSERT INTO dm.total_certificates (id, total_certificates, ranking)
-SELECT e.id
-, COALESCE(tc.total_certificates, 0) AS total_certificates
-, DENSE_RANK() OVER (ORDER BY COALESCE(tc.total_certificates, 0) DESC) AS ranking
-FROM dds.employee e
-LEFT JOIN (SELECT ec.user_id
-, COUNT(*) AS total_certificates
-FROM dds.employee_certificate ec
-GROUP BY ec.user_id) tc ON e.id = tc.user_id
-ORDER BY 2 DESC;
-'''}
-
-SQL_DM_dict['personal_data'] = '''
+SQL_DM_dict = {'personal_data':'''
 TRUNCATE TABLE dm.personal_data; INSERT INTO dm.personal_data (id, name, surname, email, frc, position, ranking)
 SELECT emp.id, emp.name, emp.surname, emp.email, emp.frc, emp."position", tc.ranking
 FROM dds.employee emp
@@ -51,13 +38,14 @@ LEFT JOIN (
 		, COUNT(*) AS total_certificates
 		FROM dds.employee_certificate ec
 		GROUP BY ec.user_id) tc ON e.id = tc.user_id ) tc ON tc.id = emp.id
-'''
-SQL_DM_dict['employee_skill'] = ''' TRUNCATE TABLE dm.employee_skill; INSERT INTO dm.employee_skill (id, 
-skill, skill_detail, grade_level_exp, sort)
- SELECT e.id,
-    'dbms'::text AS skill,
-    d.dbms AS skill_detail,
-    g.grade as grade_level_exp,
+'''}
+
+SQL_DM_dict['employee_skill'] = ''' TRUNCATE TABLE dm.employee_skill; INSERT INTO dm.employee_skill (employee_id,
+skill_type, skill_name, grade_name, sort)
+ SELECT e.id AS employee_id,
+    'dbms'::text AS skill_type,
+    d.dbms AS skill_name,
+    g.grade as grade_name,
     deg.sort
    FROM dds.employee e
     -- только максимальные грейды  по каждой базе данных--
@@ -70,9 +58,9 @@ skill, skill_detail, grade_level_exp, sort)
 
 UNION
  
- SELECT e.id,
-    'program'::text AS skill,
-    p.program AS skill_detail,
+ SELECT e.id AS employee_id,
+    'program'::text AS skill_type,
+    p.program AS skill_name,
     g.grade,
     peg.sort
    FROM dds.employee e
@@ -86,9 +74,9 @@ UNION
 
 UNION
 
-SELECT e.id,
-    'platform'::text AS skill,
-    pl.platform AS skill_detail,
+SELECT e.id AS employee_id,
+    'platform'::text AS skill_type,
+    pl.platform AS skill_name,
     g.grade,
     pleg.sort
    FROM dds.employee e
@@ -102,9 +90,9 @@ SELECT e.id,
 
 UNION
 
-SELECT e.id,
-    'tool'::text AS skill,
-    t.tool AS skill_detail,
+SELECT e.id AS employee_id,
+    'tool'::text AS skill_type,
+    t.tool AS skill_name,
     g.grade,
     g.sort
    FROM dds.employee e
@@ -118,9 +106,9 @@ SELECT e.id,
 
 UNION
 
-SELECT e.id,
-    'framework'::text AS skill,
-    f.framework AS skill_detail,
+SELECT e.id AS employee_id,
+    'framework'::text AS skill_type,
+    f.framework AS skill_name,
     g.grade,
     g.sort
    FROM dds.employee e
@@ -133,38 +121,10 @@ SELECT e.id,
      LEFT JOIN dds.grade g ON g.id = feg.grade
 
 UNION
-
-SELECT e.id,
-    'domain'::text AS skill,
-    d.domain AS skill_detail,
-    exp.experience,
-    exp.sort
-   FROM dds.employee e
-      -- только максимальные грейды  по каждой предметной области--
-	 LEFT JOIN (SELECT * FROM dds.employee_domain_experience
-				WHERE domain IS NOT NULL AND id IN
-				(SELECT MAX(id) FROM dds.employee_domain_experience
-				GROUP BY user_id, domain)) ede ON e.id = ede.user_id
-     LEFT JOIN dds.domain d ON d.id = ede.domain
-     LEFT JOIN dds.experience exp ON exp.id = ede.experience
-
-UNION
-
-SELECT e.id,
-    'industry'::text AS skill,
-    i.industry AS skill_detail,
-    iexp.experience,
-    iexp.sort
-   FROM dds.employee e
-     LEFT JOIN dds.industry_employee_experience iee ON e.id = iee.user_id
-     LEFT JOIN dds.industry i ON i.id = iee.industry
-     LEFT JOIN dds.industry_experience iexp ON iexp.id = iee.experience
-
-UNION
  
- SELECT e.id,
-    'sde'::text AS skill,
-    s.sde AS skill_detail,
+ SELECT e.id AS employee_id,
+    'sde'::text AS skill_type,
+    s.sde AS skill_name,
     g.grade,
     seg.sort
    FROM dds.employee e
